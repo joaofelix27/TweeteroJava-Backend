0227800e06ce21e3992b9bdcb35320f24777b8ac
joaofelix <joaofelix27@gmail.com>
2023-04-10 14:44:19 -0300

    get tweets with pagination

--- src/main/java/com/tweetero/api/controllers/TweetsController.java
+++ src/main/java/com/tweetero/api/controllers/TweetsController.java
@@ -1,8 +1,13 @@
 package com.tweetero.api.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweetero.api.dto.TweetsDTO;
@@ -36,6 +42,20 @@ public class TweetsController {
             return ResponseEntity.status(HttpStatus.OK).body("Tweet created successfully");
         } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User does not exist");
+        }
+    }
+
+    @GetMapping("/tweets")
+    public List<Tweets> getAllTweets(@RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "5") Integer size) {
+        Pageable paging = PageRequest.of(page, size, Sort.by("id").descending());
+
+        Page<Tweets> pagedResult = repository.findAll(paging);
+
+        if (pagedResult.hasContent()) {
+            return pagedResult.getContent();
+        } else {
+            return new ArrayList<Tweets>();
         }
     }
 
